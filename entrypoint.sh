27206d5c93b5a38b2eed89264dcd837bea957ded
Frank Spijkerman <fspijkerman@gmail.com>
2021-12-08 20:55:45 +0100

    matchers?

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,14 @@
 mkdir ._actionshub_problem-matchers
 cp /cookstyle.json ._actionshub_problem-matchers/cookstyle.json
 cp /rspec.json ._actionshub_problem-matchers/rspec.json
-echo "::[add-matcher]._actionshub_problem-matchers/cookstyle.json"
-echo "::[add-matcher]._actionshub_problem-matchers/rspec.json"
+echo "##[add-matcher]._actionshub_problem-matchers/cookstyle.json"
+echo "##[add-matcher]._actionshub_problem-matchers/rspec.json"
 if [ -n "${INPUT_GEMS}" ] ; then
   echo "Installing gem(s): ${INPUT_GEMS}"
   chef gem install -N "${INPUT_GEMS}"
 fi
 
-export
-
+echo "Checking if any work needs to be done..."
 while read cookbook; do
   echo "::group::${cookbook}"
   chef exec cookstyle --display-cop-names --extra-details ${cookbook}
