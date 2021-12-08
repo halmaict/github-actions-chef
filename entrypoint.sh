3cf93e587eb2f773af160cc5d0ed2b9a343593bf
Frank Spijkerman <fspijkerman@gmail.com>
2021-12-08 20:33:46 +0100

    scanning multiple cookbooks

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 mkdir ._actionshub_problem-matchers
 cp /cookstyle.json ._actionshub_problem-matchers/cookstyle.json
 cp /rspec.json ._actionshub_problem-matchers/rspec.json
@@ -7,4 +9,12 @@ if [ -n "${INPUT_GEMS}" ] ; then
   echo "Installing gem(s): ${INPUT_GEMS}"
   chef gem install -N "${INPUT_GEMS}"
 fi
-chef exec delivery local all
+
+export
+
+while read cookbook; do
+  echo "::group::${cookbook}"
+  chef exec cookstyle --display-cop-names --extra-details ${cookbook}
+  echo "::endgroup::"
+done < <(git diff --name-only ${GITHUB_BASE_REF} ${GITHUB_SHA} cookbooks/| cut -d"/" -f1,2 | sort -u)
+
